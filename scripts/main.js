6d4b0d1c6f27bdb0ce62f90f598dc8507d725fdf
Aarav Chowbey <aaravchowbey@gmail.com>
2025-03-25 13:21:18 -0400

    changed to just an index file for now

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+
+// this is a test script that prints to console whenever the site has loaded
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Website loaded!");
   });
