cbdf7712e6709a365599b00d541a629e7d976ac9
Aarav Chowbey <aaravchowbey@gmail.com>
2025-08-28 12:54:44 -0400

    restarting

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-
-// this is a test script that prints to console whenever the site has loaded
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Website loaded!");
-  });
-  
